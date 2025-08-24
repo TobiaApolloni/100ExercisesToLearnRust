e5fae8932ab453832b2f47ad419060001cefe65b
TobiaApolloni <tobia.apolloni@gmail.com>
2025-08-24 15:09:14 +0200

    Solved 06_ticket_managment/16_btreemap

--- exercises/06_ticket_management/16_btreemap/src/lib.rs
+++ exercises/06_ticket_management/16_btreemap/src/lib.rs
@@ -3,7 +3,10 @@
 //  references to the tickets, ordered by their `TicketId`.
 //  Implement additional traits on `TicketId` if needed.
 
+use std::collections::binary_heap::Iter;
+use std::collections::btree_map::Values;
 use std::collections::BTreeMap;
+use std::io::IntoInnerError;
 use std::ops::{Index, IndexMut};
 use ticket_fields::{TicketDescription, TicketTitle};
 
@@ -13,7 +16,7 @@ pub struct TicketStore {
     counter: u64,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct TicketId(u64);
 
 #[derive(Clone, Debug, PartialEq)]
@@ -30,7 +33,7 @@ pub struct TicketDraft {
     pub description: TicketDescription,
 }
 
-#[derive(Clone, Debug, Copy, PartialEq, Eq)]
+#[derive(Clone, Debug, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Status {
     ToDo,
     InProgress,
@@ -40,7 +43,7 @@ pub enum Status {
 impl TicketStore {
     pub fn new() -> Self {
         Self {
-            tickets: todo!(),
+            tickets: BTreeMap::new(),
             counter: 0,
         }
     }
@@ -54,16 +57,25 @@ impl TicketStore {
             description: ticket.description,
             status: Status::ToDo,
         };
-        todo!();
+        self.tickets.insert(id, ticket);
         id
     }
 
     pub fn get(&self, id: TicketId) -> Option<&Ticket> {
-        todo!()
+        self.tickets.get(&id)
     }
 
     pub fn get_mut(&mut self, id: TicketId) -> Option<&mut Ticket> {
-        todo!()
+        self.tickets.get_mut(&id)
+    }
+}
+
+impl<'a> IntoIterator for &'a TicketStore {
+    type Item = &'a Ticket;
+    type IntoIter = Values<'a, TicketId, Ticket>;
+
+    fn into_iter(self) -> Values<'a, TicketId, Ticket> {
+        self.tickets.values()
     }
 }
 
